34ecd17485baa2f18135506595ad25f91a4d16aa
Stepan <neverliberty@gmail.com>
2017-12-31 23:32:07 +0700

    Добавить файлы путем загрузки.

---
+++ Emitter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour {
+
+	public Transform[] emitters;
+	public GameObject toEmit;
+	public float delay;
+
+	void Start () {
+		StartCoroutine ("Emit");
+	}
+
+	IEnumerator Emit() {
+		while (true) {
+			yield return new WaitForSeconds (delay);
+			int i = Random.Range (0, emitters.Length);
+			Transform whereToSpawn = emitters [i];
+			Instantiate (toEmit, whereToSpawn.position, Quaternion.Euler (new Vector3 (0f, 0f, Random.Range (0f, 180f))));
+		}
+	}
+}
---
+++ Snow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snow : MonoBehaviour {
+	
+	void OnCollisionEnter2D (Collision2D col) {
+		if (col.collider.tag == "Finish") {
+			Destroy (gameObject);
+		}
+	}
+}
